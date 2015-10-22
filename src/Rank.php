635653e3e17c06b346f27811277abceb77ed9cfa
opeyemiabiodun <opeyemi.ogunjimi@andela.com>
2015-10-22 10:04:06 +0100

    Make the following corrections;
    -Use more Exceptions throughout the package

--- src/Rank.php
+++ src/Rank.php
@@ -2,7 +2,7 @@
 
 namespace League\UrbanDictionary;
 
-use \Exception;
+use Exception;
 
 class Rank
 {
@@ -16,11 +16,13 @@ class Rank
         if (!is_string($string)) {
             throw new Exception("The argument is not a string.");
         }
+
         $array = preg_split("(\s)", preg_replace("([^\w\s\-\'])", "", strtolower($string)));
         $result = array();
         foreach ($array as $value) {
             $result[$value] = (array_key_exists($value, $result)) ? $result[$value] + 1 : 1 ;
         }
+
         return $result;
     }
 }
