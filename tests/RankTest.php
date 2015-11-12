00821c810a6d2d8e3c969c079892c3f8dc87355e
Ogunjimi Opeyemi Abiodun <opeyemi.ogunjimi@andela.com>
2015-11-12 10:28:21 +0100

    Update RankTest.php

--- tests/RankTest.php
+++ tests/RankTest.php
@@ -10,8 +10,10 @@ class RankTest extends \PHPUnit_Framework_TestCase
     {
         $string = "Andrei: Prosper, Have you finished the curriculum?.\nProsper: Yes.\nAndrei: Tight, Tight, Tight!!!";
         $result = Rank::execute($string);
+        
         $this->assertTrue(is_array($result), "Error: 'WordRank::rank' should return an array.");
         $this->assertEquals(str_word_count($string), array_sum($result), 'Error: the number of words in the string must equal the sum of the values in the returned array');
+        
         foreach ($result as $key => $value) {
             $this->assertNotEquals(false, strripos($string, $key), 'Error: a key in the returned array is not contained in the string passed into WordRank::rank');
             $this->assertNotEquals(0, $value, 'Error: no element in the returned array should have a value of 0');
